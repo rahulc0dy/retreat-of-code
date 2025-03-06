e3df5961cc956ca892172210202bea0db16aed2d
rahulc0dy <rahulchakraborty337@gmail.com>
2025-03-06 17:04:02 +0530

    chore(icons, manifest): added icons and manifest files

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,27 @@ const firaCode = Fira_Code({
 export const metadata: Metadata = {
   title: "Retreat of Code",
   description: "This is absolutely not advent of code 😉",
+  icons: {
+    icon: [
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon.ico", sizes: "48x48", type: "image/x-icon" },
+    ],
+    apple: "/icons/apple-touch-icon.png",
+  },
+  category: "Technology",
+  creator: "rahulc0dy",
+  manifest: "/manifest.json",
+  keywords: [
+    "Retreat of Code",
+    "Advent of Code",
+    "puzzle",
+    "coding challenge",
+    "programming puzzle",
+    "daily puzzle",
+    "holiday coding challenge",
+    "software challenge",
+  ],
 };
 
 export default function RootLayout({
